9b67b5ebe16e3c03d20f1c82061bf2d9ca2e92b0
jonasmaertens <jojojojo2803@gmail.com>
2023-10-26 12:29:21 +0200

    non blocking plots + example trainings

--- TASEP-Smarticles/hyperparam_optim.py
+++ TASEP-Smarticles/hyperparam_optim.py
@@ -2,18 +2,23 @@ from Trainer import Trainer, Hyperparams, EnvParams
 import itertools
 from tqdm import tqdm
 import datetime
+import os
 
 observation_distances = [1]
 batch_sizes = [512]
 gammas = [0.3, 0.6, 0.999]
 eps_starts = [0.9]
-eps_ends = [0.1]
-eps_decays = [5_000, 100_000, 600_000]
+eps_ends = [0.1, 0.01]
+eps_decays = [5_000, 10_000, 100_000, 600_000]
 taus = [1e-4, 1e-2]
 lrs = [1e-3, 1e-2, 1e-1]
 memory_sizes = [100_000]
 
 if __name__ == '__main__':
+    dirs = ["models/hyperparam_tune_disting", "plots/hyperparam_tune_disting", "data/hyperparam_tune_disting"]
+    for directory in dirs:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
     timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     # with open(f"data/hyperparam_tune/models{timestamp}.csv", "w") as f:
     #     f.write("observation_distance,batch_size,gamma,eps_start,eps_end,eps_decay,tau,lr,memory_size,current\n")
@@ -29,7 +34,7 @@ if __name__ == '__main__':
                               window_height=256,
                               observation_distance=observation_distance,
                               initial_state_template="checkerboard",
-                              distinguishable_particles=False)
+                              distinguishable_particles=True)
         hyperparams = Hyperparams(BATCH_SIZE=batch_size,
                                   GAMMA=gamma,
                                   EPS_START=eps_start,
@@ -44,13 +49,14 @@ if __name__ == '__main__':
 
         trainer.train()
 
+
         trainer.save(
-            f"models/hyperparam_tune/example_model_{observation_distance}_{batch_size}_{gamma}_{eps_start}_{eps_end}_{eps_decay}_{tau}_{lr}_{memory_size}.pt")
+            f"models/hyperparam_tune_disting/example_model_{observation_distance}_{batch_size}_{gamma}_{eps_start}_{eps_end}_{eps_decay}_{tau}_{lr}_{memory_size}.pt")
 
         trainer.save_plot(
-            f"plots/hyperparam_tune/example_model_{observation_distance}_{batch_size}_{gamma}_{eps_start}_{eps_end}_{eps_decay}_{tau}_{lr}_{memory_size}.png",
+            f"plots/hyperparam_tune_disting/example_model_{observation_distance}_{batch_size}_{gamma}_{eps_start}_{eps_end}_{eps_decay}_{tau}_{lr}_{memory_size}.png",
             append_timestamp=False)
 
-        with open(f"data/hyperparam_tune/models{timestamp}.csv", "a") as f:
+        with open(f"data/hyperparam_tune_disting/models{timestamp}.csv", "a") as f:
             f.write(
                 f"{observation_distance},{batch_size},{gamma},{eps_start},{eps_end},{eps_decay},{tau},{lr},{memory_size},{trainer.currents[-1]}\n")
