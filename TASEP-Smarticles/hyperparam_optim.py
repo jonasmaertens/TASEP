523677b5047c9125b1d9d539fa59594cd5fdc65e
Jonas Märtens <jojojojo2803@gmail.com>
2023-10-26 10:10:56 +0200

    first tuning results part1

--- TASEP-Smarticles/hyperparam_optim.py
+++ TASEP-Smarticles/hyperparam_optim.py
@@ -3,23 +3,25 @@ import itertools
 from tqdm import tqdm
 import datetime
 
-observation_distances = [1, 2]
-batch_sizes = [512, 8192]
+observation_distances = [1]
+batch_sizes = [512]
 gammas = [0.3, 0.6, 0.999]
 eps_starts = [0.9]
 eps_ends = [0.1]
 eps_decays = [5_000, 100_000, 600_000]
 taus = [1e-4, 1e-2]
 lrs = [1e-3, 1e-2, 1e-1]
-memory_sizes = [1000, 1_000_000]
+memory_sizes = [100_000]
 
 if __name__ == '__main__':
     timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    with open(f"data/hyperparam_tune/models{timestamp}.csv", "w") as f:
-        f.write("observation_distance,batch_size,gamma,eps_start,eps_end,eps_decay,tau,lr,memory_size,current\n")
+    # with open(f"data/hyperparam_tune/models{timestamp}.csv", "w") as f:
+    #     f.write("observation_distance,batch_size,gamma,eps_start,eps_end,eps_decay,tau,lr,memory_size,current\n")
     for observation_distance, batch_size, gamma, eps_start, eps_end, eps_decay, tau, lr, memory_size in tqdm(
             itertools.product(
-                    observation_distances, batch_sizes, gammas, eps_starts, eps_ends, eps_decays, taus, lrs, memory_sizes)):
+                observation_distances, batch_sizes, gammas, eps_starts, eps_ends, eps_decays, taus, lrs, memory_sizes)):
+        if gamma == 0.3 and eps_decay == 5_000 and tau == 1e-4 and lr == 1e-3:
+            continue
         envParams = EnvParams(render_mode=None,
                               length=32,
                               width=8,
@@ -50,5 +52,5 @@ if __name__ == '__main__':
             append_timestamp=False)
 
         with open(f"data/hyperparam_tune/models{timestamp}.csv", "a") as f:
-            f.write(f"{observation_distance},{batch_size},{gamma},{eps_start},{eps_end},{eps_decay},{tau},{lr},{memory_size},{trainer.currents[-1]}\n")
-
+            f.write(
+                f"{observation_distance},{batch_size},{gamma},{eps_start},{eps_end},{eps_decay},{tau},{lr},{memory_size},{trainer.currents[-1]}\n")
