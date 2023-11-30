7d189cf5d7ff1d91719904b8020970030ae04158
jonasmaertens <jojojojo2803@gmail.com>
2023-11-30 17:25:57 +0100

    lots of clustering experiments

--- TASEP-Smarticles/src/SmartTasep/DQN.py
+++ TASEP-Smarticles/src/SmartTasep/DQN.py
@@ -16,20 +16,17 @@ class DQN(nn.Module):
             self.layer2 = nn.Linear(128, 128)
             self.layer3 = nn.Linear(128, n_actions)
         else:
-            hid1size = int(n_observations * 1.5)
-            self.layer1 = nn.Linear(n_observations, hid1size)
-            self.layer2 = nn.Linear(hid1size, 128)
-            self.layer3 = nn.Linear(128, 80)
-            self.layer4 = nn.Linear(80, n_actions)
+            self.layer1 = nn.Linear(n_observations, 24)
+            self.layer2 = nn.Linear(24, 12)
+            self.layer3 = nn.Linear(12, n_actions)
 
     # Called with either one element to determine next action, or a batch
     # during optimization. Returns tensor([[left0exp,right0exp]...]).
     def forward(self, x):
         x = F.relu(self.layer1(x))
-        x = F.relu(self.layer2(x))
-        if hasattr(self, "layer4"):
-            x = F.relu(self.layer3(x))
-            x = self.layer4(x)
-        else:
+        if hasattr(self, "layer3"):
+            x = F.relu(self.layer2(x))
             x = self.layer3(x)
+        else:
+            x = self.layer2(x)
         return x
--- TASEP-Smarticles/update_models_new_param.py
+++ TASEP-Smarticles/update_models_new_param.py
@@ -5,7 +5,7 @@ with open("models/all_models.json", "r") as f:
     all_models = json.load(f)
 for model_id in all_models:
     print(model_id)
-    all_models[model_id]["diff_models"] = False
+    all_models[model_id]["num_models"] = 1
 
     # with open(os.path.join(all_models[model_id]["path"], "env_params.json"), "w") as f:
     #     json.dump(all_models[model_id]["env_params"], f)
