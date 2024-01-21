95b81a6c4a07f8db53a0f320f6ba9fcc3d0e19f2
jonasmaertens <jojojojo2803@gmail.com>
2024-01-21 18:28:04 +0100

    add gif

--- TASEP-Smarticles/example_run_pretrained.py
+++ TASEP-Smarticles/example_run_pretrained.py
@@ -4,7 +4,7 @@ from Trainer import Trainer
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    trainer = Trainer.load(model_id=41, do_plot=False, total_steps=1_500_010, average_window=2000)
+    trainer = Trainer.load(model_id=34, do_plot=True, total_steps=1_500_010, average_window=2000)
     # trainer.env.unwrapped.render_mode = None
     trainer.run(reset_stats=True)
-    trainer.save_run_data_for_plot(name="uniform_speeds")
+    # trainer.save_run_data_for_plot(name="uniform_speeds")
