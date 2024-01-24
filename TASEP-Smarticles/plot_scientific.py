90f161d4f733b4a9a30dea97296cc0c8bb049725
jonasmaertens <jojojojo2803@gmail.com>
2024-01-24 20:34:52 +0100

    thesis progress

--- TASEP-Smarticles/plot_scientific.py
+++ TASEP-Smarticles/plot_scientific.py
@@ -9,15 +9,20 @@ plt.rcParams['figure.figsize'] = (4.5, 3.2)
 
 
 # Load data
-name = "uniform_speeds"
+name = "speed_grad_4"
 timesteps = np.load(f"data/{name}/timesteps.npy")
 currents = np.load(f"data/{name}/currents.npy")
 
-plot_interval = 2000
+# smooth currents
+currents = np.convolve(currents, np.ones(10)/10, mode='valid')
+timesteps = timesteps[4:-5]
+
+plot_interval = 5000
 plt.xlabel("Time steps")
 plt.ylabel(f"Current")
-plt.plot(timesteps, currents, label=f"Data (last {plot_interval} steps averaged)")
-plt.plot(timesteps, np.ones(len(timesteps)) * np.mean(currents[125:]),
-         label=f"Average (first {plot_interval * 125} steps excluded)")
+plt.plot(timesteps, currents, label=f"Data (moving average of 50,000 steps)")
+plt.plot(timesteps, np.ones(len(timesteps)) * np.mean(currents[-300:]),
+         label=f"Average over last 1,500,000 steps")
 plt.legend()
 plt.savefig(f"plots/scientific/{name}.pdf")
+plt.show()
