59ba0a1efb0ec8f62c67104716ddbf0cd1dad9d1
jonasmaertens <jojojojo2803@gmail.com>
2024-01-24 20:34:52 +0100

    thesis progress

--- TASEP-Smarticles/example_run_pretrained.py
+++ TASEP-Smarticles/example_run_pretrained.py
@@ -1,7 +1,7 @@
 from smarttasep import Trainer
 
 if __name__ == '__main__':
-    trainer = Trainer.load(model_id=6, do_plot=True, total_steps=1_500_010, average_window=2000)
+    trainer = Trainer.load(model_id=7, do_plot=True, total_steps=8_500_010, average_window=5000, wait_initial=5)
     # trainer.env.unwrapped.render_mode = None
     trainer.run(reset_stats=True)
-    # trainer.save_run_data_for_plot(name="uniform_speeds")
+    trainer.save_run_data_for_plot(name="speed_grad_4")
