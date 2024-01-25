486cb60f7c6f74d779ca633fd0418df435d18d7a
jonasmaertens <jojojojo2803@gmail.com>
2024-01-25 13:51:00 +0100

    thesis progress

--- TASEP-Smarticles/example_run_pretrained.py
+++ TASEP-Smarticles/example_run_pretrained.py
@@ -1,7 +1,7 @@
 from smarttasep import Trainer
 
 if __name__ == '__main__':
-    trainer = Trainer.load(model_id=34, do_plot=True, total_steps=8_500_010, average_window=5000, wait_initial=0)
+    trainer = Trainer.load(model_id=10, do_plot=True, total_steps=1_500_010, average_window=5000, wait_initial=0)
     # trainer.env.unwrapped.render_mode = None
     trainer.run(reset_stats=True)
-    # trainer.save_run_data_for_plot(name="speed_grad_4")
+    trainer.save_run_data_for_plot(name="lanes_1")
--- TASEP-Smarticles/plot_scientific.py
+++ TASEP-Smarticles/plot_scientific.py
@@ -9,20 +9,20 @@ plt.rcParams['figure.figsize'] = (4.5, 3.2)
 
 
 # Load data
-name = "speed_grad_4"
+name = "lanes_1"
 timesteps = np.load(f"data/{name}/timesteps.npy")
 currents = np.load(f"data/{name}/currents.npy")
 
 # smooth currents
-currents = np.convolve(currents, np.ones(10)/10, mode='valid')
-timesteps = timesteps[4:-5]
+currents = np.convolve(currents, np.ones(20)/20, mode='valid')
+timesteps = timesteps[9:-10]
 
 plot_interval = 5000
 plt.xlabel("Time steps")
 plt.ylabel(f"Current")
-plt.plot(timesteps, currents, label=f"Data (moving average of 50,000 steps)")
-plt.plot(timesteps, np.ones(len(timesteps)) * np.mean(currents[-300:]),
-         label=f"Average over last 1,500,000 steps")
+plt.plot(timesteps, currents, label=f"Data (100,000 steps moving avg.)")
+plt.plot(timesteps, np.ones(len(timesteps)) * np.mean(currents),
+         label=f"Average")
 plt.legend()
 plt.savefig(f"plots/scientific/{name}.pdf")
-plt.show()
+# plt.show()
